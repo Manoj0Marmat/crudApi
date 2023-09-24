44cbc346b1294fa0a3ca7f1a628bd4d198834869
Manoj Marmat <manojrajput762000@gmail.com>
2023-09-24 21:49:11 +0530

    feat(update): add update program details method

--- API/Controllers/ProgramDetailController.cs
+++ API/Controllers/ProgramDetailController.cs
@@ -15,26 +15,49 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProgramDetail>>> GetProgramDetails()
+        public async Task<ActionResult<IReadOnlyList<ProgramDetail>>> GetAllProgramDetail()
         {
             return Ok(await _programDetailRepository.GetAllProgramDetailsAsync());
         }
 
         [HttpGet("{id}")]
-        public async Task<IReadOnlyList<ProgramDetail>> GetProgramDetail(int id)
+        public async Task<ActionResult<ProgramDetail>> GetProgramDetail(int id)
         {
-
+            return Ok(await _programDetailRepository.GetProgramDetailByIdAsync(id));
         }
 
         [HttpPost]
-        public async Task<ProgramDetail> AddProgramDetails(ProgramDetail programDetail)
+        public async Task<ProgramDetail> AddProgramDetail(ProgramDetail programDetail)
         {
             await _programDetailRepository.CreateProgramDetailAsync(programDetail);
             return programDetail;
         }
 
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ProgramDetail>> UpdateProgramDetail(int id, ProgramDetail programDetail)
+        {
+            var existingProgramDetail = await _programDetailRepository.GetProgramDetailByIdAsync(id);
+            
+            if (existingProgramDetail == null)
+            {
+                return NotFound();
+            }
+
+            existingProgramDetail.ProgramTitle = programDetail.ProgramTitle;
+            existingProgramDetail.Summary = programDetail.Summary;
+            existingProgramDetail.Description = programDetail.Description;
+            existingProgramDetail.KeySkills = programDetail.KeySkills;
+            existingProgramDetail.Benefits = programDetail.Benefits;
+            existingProgramDetail.ApplicationCriteria = programDetail.ApplicationCriteria;
+
+            var updatedProgramDetail = await _programDetailRepository.UpdateProgramDetailAsync(existingProgramDetail);
+            
+            return Ok(updatedProgramDetail);
+        }
+
+
         [HttpDelete]
-        public async Task DeleteProgramDetails(int id)
+        public async Task DeleteProgramDetail(int id)
         {
             await _programDetailRepository.DeleteProgramDetailAsync(id);
         }
